d8ff30dad004439cf77fca74b842dd616bd05e01
alesslu <alessluchata@gmail.com>
2023-01-13 16:38:36 -0500

    agregando final

--- negocio-main/src/app/categories/categories.component.ts
+++ negocio-main/src/app/categories/categories.component.ts
@@ -10,12 +10,16 @@ export class CategoriesComponent {
   {id:"2", name:"Groceries", icon:"fas fa-shopping-cart", type:"papel", color:"borde-6"},
   {id:"3", name:"Transport", icon:"fas fa-car", type:"papel", color:"borde-2"},
   {id:"4", name:"Health", icon:"fas fa-medkit", type:"papel", color:"borde-1"},
-  {id:"4", name:"Gifts", icon:"fas fa-gift", type:"papel", color:"borde-10"},
-  {id:"4", name:"Education", icon:"fa-solid fa-book", type:"papel", color:"borde-7"}]
+  {id:"5", name:"Gifts", icon:"fas fa-gift", type:"papel", color:"borde-10"},
+  {id:"6", name:"Education", icon:"fa-solid fa-book", type:"papel", color:"borde-7"}]
 
   name: string ="Rent"
 
-
+  addNewCategory(category: any): void {
+    console.log(category);
+    this.cards.push(category);
+  }
 
 
 }
+
--- negocio-main/src/app/modal/modal.component.ts
+++ negocio-main/src/app/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ElementRef } from '@angular/core';
 
 declare var window: any;
 
@@ -20,9 +20,33 @@ interface modal {
 export class ModalComponent implements OnInit {
 
   formModal: any;
-  categories = {id:"", name:"", icon:"", type:"", color:""};
+  categories: {id: string, name: string, icon: string, type: string, color: string} = {id: "", name: "", icon: "", type: "", color: ""};
   aux=7;
   id=""+this.aux;
+  @Output() newCategory: EventEmitter<any> = new EventEmitter();
+
+  public colores: { class: string, color: string, clicked: boolean }[] = [
+    { class: 'btn btn-1 btn-circle', color: 'borde-1', clicked: false },
+    { class: 'btn btn-2 btn-circle', color: 'borde-2', clicked: false },
+    { class: 'btn btn-3 btn-circle', color: 'borde-3', clicked: false },
+    { class: 'btn btn-4 btn-circle', color: 'borde-4', clicked: false },
+    { class: 'btn btn-5 btn-circle', color: 'borde-5', clicked: false },
+    { class: 'btn btn-6 btn-circle', color: 'borde-6', clicked: false },
+    { class: 'btn btn-7 btn-circle', color: 'borde-7', clicked: false },
+    { class: 'btn btn-8 btn-circle', color: 'borde-8', clicked: false },
+  ]
+
+  public iconos: { icon: string, clicked: boolean }[] = [
+    { icon: 'fa fa-university', clicked: false },
+    { icon: 'fas fa-shopping-cart', clicked: false },
+    { icon: 'fas fa-medkit', clicked: false },
+    { icon: 'fas fa-gamepad', clicked: false },
+    { icon: 'fas fa-graduation-cap', clicked: false },
+    { icon: 'fas fa-gift', clicked: false },
+    { icon: 'fas fa-car', clicked: false },
+    { icon: 'fa-solid fa-book', clicked: false },
+  ]
+
   constructor() {}
 
  
@@ -36,24 +60,41 @@ export class ModalComponent implements OnInit {
     this.formModal.show();
   }
   agregarColor(color:string):void{
-    this.categories.color = color
-
+    this.categories!.color = color
+    this.setClickedColor(color);
   }
   agregarIcono(icon:string):void{
-    this.categories.icon = icon
-
+    this.categories!.icon = icon
+    this.setClickedIcon(icon);
   }
 
   agregarName(name:string):void{
-    this.categories.name = name
+    this.categories!.name = name
     
   }
   saveSomeThing(){
     this.formModal.hide();
-    this.categories.id = this.id;
-    console.log(this.categories)
+    this.categories!.id = this.id;
     this.aux++;
     this.id=""+this.aux;
+    this.newCategory.emit(this.categories);
+    this.categories = {id: "", name: "", icon: "", type: "", color: ""};
+  }
+
+  setClickedColor(color: string) {
+    this.colores.forEach((item: any) => {
+      item.clicked = false;
+    })
+    let current = this.colores.filter((x: any) => x.color == color)[0];
+    current.clicked = true;
+  }
+
+  setClickedIcon(icon: string) {
+    this.iconos.forEach((item: any) => {
+      item.clicked = false;
+    })
+    let current = this.iconos.filter((x: any) => x.icon == icon)[0];
+    current.clicked = true;
   }
   
   
